09138be1d5affa6aec52a171d37fa7f15992526c
maxdegers <maxdegers@gmail.com>
2023-11-10 18:46:45 +0100

    added

--- ft_lstadd_back.c
+++ ft_lstadd_back.c
@@ -6,7 +6,7 @@
 /*   By: mbrousse <mbrousse@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/10 18:23:30 by mbrousse          #+#    #+#             */
-/*   Updated: 2023/11/10 18:33:11 by mbrousse         ###   ########.fr       */
+/*   Updated: 2023/11/10 18:43:42 by mbrousse         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,14 @@
 
 void ft_lstadd_back(t_list **lst, t_list *new)
 {
-    t_list  *lpt;
-    
-    lpt = ft_lstlast(*lst);
-    lpt->next = new;
+    t_list *pt;
+
+    if (!*lst)
+        return ;
+    pt = *lst;
+    while (pt->next != NULL)
+    {
+        pt = pt->next;
+    }
+    pt->next = new;
 }
--- ft_lstlast.c
+++ ft_lstlast.c
@@ -6,7 +6,7 @@
 /*   By: mbrousse <mbrousse@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/10 18:18:55 by mbrousse          #+#    #+#             */
-/*   Updated: 2023/11/10 18:21:30 by mbrousse         ###   ########.fr       */
+/*   Updated: 2023/11/10 18:45:06 by mbrousse         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,17 +14,14 @@
 
 t_list *ft_lstlast(t_list *lst)
 {
-        int i;
     t_list *pt;
 
     if (!lst)
         return (0);
-    i = 1;
     pt = lst;
     while (pt->next)
     {
         pt = pt->next;
-        i++;
     }
     return (pt);
 }
